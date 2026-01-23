d8e37407c9f3b6b4f9889727bc4360655873c3ac
Aloïs Duguet <duguet@uni-trier.de>
2026-01-23 22:38:51 +0100

    slideElement added from top to bottom in a GroupWindow instance

--- MusicRanking/groupWindow.py
+++ MusicRanking/groupWindow.py
@@ -6,9 +6,13 @@ from slideElement import *
 class GroupWindow:
     def __init__(self, n):
         self.nSlideElements = n # number of slideElements
+        self.root = tk.Tk()
         self.slideElements = []
         for i in range(n):
-            self.slideElements.append(SlideElement())
+            self.slideElements.append(SlideElement(self.root, i))
+            self.slideElements[-1].frame.pack()
         
         # position elements from top to bottom
-        
+
+window = GroupWindow(3)
+window.root.mainloop()
