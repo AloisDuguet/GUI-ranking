53084309a6d8b8f1a0f24a941882a2090a5fee69
Aloïs Duguet <duguet@uni-trier.de>
2026-01-24 16:43:54 +0100

    colors for buttons pressed is working as intended: deselection possible, ..."

--- MusicRanking/DemotePromoteWindow.py
+++ MusicRanking/DemotePromoteWindow.py
@@ -5,20 +5,39 @@ class DemotePromoteWindow(GroupWindow):
     def __init__(self, nameList):
         GroupWindow.init(self, nameList, DemotePromoteElement, self.promote, self.demote)
         self.slideElements[0].up.state(['!disabled'])
-        self.slideElements[0].down.state(['!disabled'])
+        self.slideElements[self.nSlideElements-1].down.state(['!disabled'])
         self.toBePromoted = []
         self.toBeDemoted = []
+
+        # define styles depending on whose option is selected
+        style = ttk.Style()
+        style.configure("Promoted.TButton", background="green")
+        style2 = ttk.Style()
+        style2.configure("Demoted.TButton", background="red")
+        style3 = ttk.Style()
+        style3.configure("notPressed.TButton")
     
     def promote(self, element):
-        element.toBePromoted = True
-        element.toBeDemoted = False
-        print(f"{element.getName()} will be promoted")
-        # TODO: add a color to button when True
+        if element.toBePromoted == False:
+            element.toBePromoted = True
+            element.toBeDemoted = False
+            print(f"{element.getName()} will be promoted")
+            element.up.config(style="Promoted.TButton")
+            element.down.config(style="notPressed.TButton")
+        else:
+            element.toBePromoted = False
+            element.up.config(style="notPressed.TButton")
 
     def demote(self, element):
-        element.toBePromoted = False
-        element.toBeDemoted = True
-        print(f"{element.getName()} will be demoted")
+        if element.toBeDemoted == False:
+            element.toBePromoted = False
+            element.toBeDemoted = True
+            print(f"{element.getName()} will be demoted")
+            element.down.config(style="Demoted.TButton")
+            element.up.config(style="notPressed.TButton")
+        else:
+            element.toBeDemoted = False
+            element.down.config(style="notPressed.TButton")
 
     def validateRanking(self):
         toStay = []
