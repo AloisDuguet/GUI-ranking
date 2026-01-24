8ada5d051ef271d27877af501d6b0fd4861c7a1f
Aloïs Duguet <duguet@uni-trier.de>
2026-01-24 11:51:41 +0100

    names instead of just number of slide elements working

--- MusicRanking/groupWindow.py
+++ MusicRanking/groupWindow.py
@@ -4,16 +4,15 @@ from tkinter import ttk
 from slideElement import *
 
 class GroupWindow:
-    def __init__(self, n):
-        self.nSlideElements = n # number of slideElements
+    def __init__(self, nameList):
+        self.nSlideElements = len(nameList) # number of slideElements
         self.root = tk.Tk()
         self.root.columnconfigure(0,weight=1)
-        self.root.rowconfigure(n, weight=1)
+        self.root.rowconfigure(self.nSlideElements, weight=1)
         self.slideElements = []
-        for i in range(n):
-            self.slideElements.append(SlideElement(self.root, i, self.goUp, self.goDown))
+        for i in range(self.nSlideElements):
+            self.slideElements.append(SlideElement(self.root, nameList[i], self.goUp, self.goDown))
             self.slideElements[-1].frame.grid(column=0, row=i)
-        #print(self.slideElements[-1].configure().keys())
     
     def goUp(self, slideElement):
         indexClick = self.slideElements.index(slideElement)
@@ -45,24 +44,7 @@ class GroupWindow:
             print(f"slide element {indexClick} is not allowed to "
                   "go down once")
 
-    def switchPlace(self, slideElement):
-        indexClick = self.slideElements.index(slideElement)
-        print(f"index of slide element with position modified: {indexClick}")
-        # I want to access the position but I don't know how
-        newPositionClick = self.slideElements[indexClick].getPosition()
-        if newPositionClick >= 0 and newPositionClick < len(self.slideElements):
-            print(f"slide element {indexClick} is allowed to "
-                  f"exchange with slide element {newPositionClick}")
-        #self.slideElements[positionClick].frame.grid(column=0, row = positionClick-1)
-        #self.slideElements[indexElementAbove].frame.grid(column=0, row = positionClick)
 
-    def getElementToExchange(self, position):
-        for i in range(n):
-            pos = self.slideElements[i].getPosition()
-            if pos == position:
-                return pos
-        print(f"element cannot move to position {position}, " 
-              "it is out of the valid positions. Put his value to the old one.")
-
-window = GroupWindow(3)
+nameList = ["comme un boomerang", "la marseillaise", "grand pianola music"]
+window = GroupWindow(nameList)
 window.root.mainloop()
