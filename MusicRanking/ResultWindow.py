0dfe4d966cde1df3d2b045ec6ba9790a79b45e67
Aloïs Duguet <duguet@uni-trier.de>
2026-03-21 13:41:45 +0100

    Text widget scrollable but close button after Text is out of window

--- MusicRanking/ResultWindow.py
+++ MusicRanking/ResultWindow.py
@@ -5,25 +5,39 @@ class ResultWindow:
     def __init__(self, root, participants):
         self.root = root
         self.participants = participants
-        self.frame = ttk.Frame(self.root,
-                               relief='raised')
+        self.text = tk.Text(self.root,
+                            height=len(self.participants),
+                            relief='raised')
 
         # fill in participants with corresponding ranking
         self.elements = []
         self.rankings = []
         for i in range(len(self.participants)):
-            self.frame.rowconfigure(i)
-            self.elements.append(ttk.Label(self.frame, text="{} - {}".format(i+1,self.participants[i]), anchor='center', width=400))
-            self.elements[i].pack()
-        
+            print("inserting one competitor at rank {} in position {}.0".format(i+1,i+1))
+            self.text.insert(index='{}.0'.format(i+1),
+                             chars="{} - {}\n".format(i+1,self.participants[i]))
+        # disable editing to keep the ranking intact
+        self.text['state'] = 'disabled'
+
+        # test coordinates in self.text
+        # print("copy of text from line 30 on:")
+        # self.copy = self.text.get('10.0',tk.END)
+        # print(type(self.copy))
+        # print(self.copy)
+            
         # add button to close this window
-        self.closeButton = ttk.Button(self.frame,
+        self.closeButton = ttk.Button(self.root,
                                       text="close ranking",
                                       command=self.root.quit)
         
+        # add vertical scrollbar
+        self.scrollbar = ttk.Scrollbar(self.root, orient=tk.VERTICAL, command=self.text.yview)
+        self.text['yscrollcommand'] = self.scrollbar.set
+        
     def showResults(self):
-        self.frame.pack()
+        self.text.pack(fill="both", expand=True)
         self.closeButton.pack()
         self.root.mainloop()
-        self.frame.destroy()
+        self.text.destroy()
+        self.closeButton.destroy()
         return 0
