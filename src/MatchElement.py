b01e6979c79a5a7caaecc24d639175b5aa5db36c
Aloïs Duguet <duguet@uni-trier.de>
2026-03-29 12:52:34 +0200

    competitors in 1v1 match occupy all planned space + qualify buttons below

--- src/MatchElement.py
+++ src/MatchElement.py
@@ -8,12 +8,12 @@ class MatchElement(SlideElement):
         self.frame = ttk.Frame(master,
                                borderwidth=3,
                                relief='raised')
-        self.frame.columnconfigure(0, weight=5)
-        self.frame.columnconfigure(1, weight=2)
-        self.frame.rowconfigure(0)
+        self.frame.rowconfigure(0, weight=2)
+        self.frame.rowconfigure(1, weight=1)
+        self.frame.columnconfigure(0, weight=1)
 
-        self.name = ttk.Label(self.frame, text=name)
-        self.name.grid(column=0, row=0)
+        self.name = ttk.Label(self.frame, text=name, anchor='center')
+        self.name.grid(row=0, sticky='news')
 
         self.qualify = ttk.Button(self.frame, text="qualify", command=lambda *args: callbackQualify(self))
-        self.qualify.grid(column=1, row=0, sticky=tk.E)
+        self.qualify.grid(row=1, sticky='news')
